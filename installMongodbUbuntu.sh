f3309fbe686d1a9030a0e9012168f7753ca69f1e
Shubham Mathur <49810353+s-mathur@users.noreply.github.com>
2021-06-18 17:18:09 +0530

    Add files via upload

---
+++ installMongodbUbuntu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x
+
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+ps --no-headers -o comm 1
+sudo systemctl start mongod
+sudo systemctl daemon-reload
+sudo systemctl status mongod
+
